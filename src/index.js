e5366f0db13691d5f615a5160a8cc8e179bce37c
karthick <karthylock@gmail.com>
2021-03-04 22:45:39 +0530

    changed files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<script src='https://cdn.jsdelivr.net/npm/big.js@6.0.0/big.min.js'></script>
 
 ReactDOM.render(
   <React.StrictMode>
